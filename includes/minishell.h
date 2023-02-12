3ea51f135d7b8c9f051609edfa13d4588315c25d
Alex Mitcul <amitcul@student.42porto.com>
2023-02-12 19:10:10 +0000

    project skeleton

---
+++ includes/minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amitcul <amitcul@student.42porto.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/14 13:47:01 by amitcul           #+#    #+#             */
+/*   Updated: 2023/02/12 17:42:48 by amitcul          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libft/includes/libft.h"
+# include "../libft/includes/ft_printf.h"
+
+# include <limits.h>
+
+# define ERROR 0
+# define OK 1
+
+#endif
