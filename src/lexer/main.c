aa5040ecc96da4cef4c3170cf6d65f09ad00e840
Alex Mitcul <amitcul@student.42porto.com>
2023-02-19 15:07:16 +0000

    gitignore fix

--- src/lexer/main.c
+++ src/lexer/main.c
@@ -6,14 +6,13 @@
 /*   By: amitcul <amitcul@student.42porto.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 22:23:05 by amitcul           #+#    #+#             */
-/*   Updated: 2023/02/19 14:15:31 by amitcul          ###   ########.fr       */
+/*   Updated: 2023/02/19 15:05:56 by amitcul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/utils.h"
 
 #include <stdio.h>
-void	dll_append(t_dll_item **head_ref, void *new_data);
 int		lexer(t_dll_item *, char *);
 
 
