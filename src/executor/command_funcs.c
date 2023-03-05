7ba6a7217a9c647c88850da218f985b3e4cbbd91
Alex Mitcul <amitcul@student.42porto.com>
2023-03-05 15:38:06 +0000

    stage

--- src/executor/command_funcs.c
+++ src/executor/command_funcs.c
@@ -6,7 +6,7 @@
 /*   By: amitcul <amitcul@student.42porto.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 15:05:07 by amitcul           #+#    #+#             */
-/*   Updated: 2023/03/05 15:33:32 by amitcul          ###   ########.fr       */
+/*   Updated: 2023/03/05 15:37:32 by amitcul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,7 @@ static bool	try_run_our_builtin(t_app *self, t_exec *exec)
 	static char	*our_functions = {
 		"echo", "cd", "pwd", "export", "unset", "env", "exit"
 	};
+	static int (*fparr[5])(void) = { func, func, func, func, func };
 	int	i;
 
 	i = 0;
